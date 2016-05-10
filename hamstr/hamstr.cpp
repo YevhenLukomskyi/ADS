1f7693c9510c8fa6b17d3327df9261412b91ac87
Yevhen Lukomskyi <Yevhen.Lukomskyi@gmail.com>
2016-05-10 08:40:56 +0300

    Revert "hamstr solution: merge sort + insertion sort"
    This reverts commit 23162c1c2916584cebcda5f85b4dd45bb45099a5.

--- hamstr/hamstr.cpp
+++ hamstr/hamstr.cpp
@@ -5,21 +5,18 @@
 #include <vector>
 #include <algorithm>
 
-struct HAMSTR {
-	int foodRate;
-	int foodGreed;
-	int foodTotal;
-};
 
 struct INPUT_DATA {
 	int foodSupplay;
 	int hamstrCount;
-	std::vector<HAMSTR> hamstrs;
+	std::vector<int> hamstrFoodRate;
+	std::vector<int> hamstrFoodGreed;
 };
 
 struct OUTPUT_DATA {
 	double maxHamstrCount;
 };
+
 
 struct INPUT_DATA readInput(std::string fileName) {
 	std::ifstream inputFile(fileName, std::ifstream::in);
@@ -31,90 +28,93 @@ struct INPUT_DATA readInput(std::string fileName) {
 	inputFile >> inputData.foodSupplay;
 	inputFile >> inputData.hamstrCount;
 
-	inputData.hamstrs = std::vector<struct HAMSTR>(inputData.hamstrCount);
+	inputData.hamstrFoodRate = std::vector<int>(inputData.hamstrCount);
+	inputData.hamstrFoodGreed = std::vector<int>(inputData.hamstrCount);
 	int inputHamstrFoodRate, inputHamstrFoodGreed;
 
 	for (int i = 0; i < inputData.hamstrCount; i++) {
 		inputFile >> inputHamstrFoodRate >> inputHamstrFoodGreed;
-		inputData.hamstrs[i].foodRate = inputHamstrFoodRate;
-		inputData.hamstrs[i].foodGreed = inputHamstrFoodGreed;
-		inputData.hamstrs[i].foodTotal = inputHamstrFoodRate;
+		inputData.hamstrFoodRate[i] = inputHamstrFoodRate;
+		inputData.hamstrFoodGreed[i] = inputHamstrFoodGreed;
 	}
 
 	return inputData;
 }
 
-int getSum(std::vector<struct HAMSTR>& vect, int n) {
+int getSum(std::vector<int>& vect, int n) {
 	int sum = 0;
 	for (int i = 0; i < n; i++) {
-		sum += vect[i].foodTotal;
+		sum += vect[i];
 	}
 	return sum;
 }
 
-void merge(std::vector<struct HAMSTR> &array, std::vector<struct HAMSTR> &aux, int left, int middle, int right) {
-	// Sedgewick's implementation of 'merge'.
+int partition(std::vector<int>& vect, int low, int high) {
+	int left = std::min(low + 1, high);
+	int right = high;
 
-	// Back up array items to 'aux'.
-	for (int k = left; k <= right; k++) {
-		aux[k] = array[k];
+	int pivot = vect[low];
+
+	while (true) {
+		while (vect[left] <= pivot) {
+			if (left == high) {
+				break;
+			}
+			left++;
+		}
+
+		while (pivot <= vect[right]) {
+			if (right == low) {
+				break;
+			}
+			right--;
+		}
+
+		if (left >= right) {
+			break;
+		}
+
+		std::swap(vect[left], vect[right]);
 	}
 
-	// Merge 'aux' back to 'array'.
-	int i = left, j = middle + 1;
-	for (int k = left; k <= right; k++) {
-		if (i > middle)            array[k] = aux[j++];
-		else if (j > right)        array[k] = aux[i++];
-		else if (aux[j].foodTotal > aux[i].foodTotal)  array[k] = aux[j++];
-		else                       array[k] = aux[i++];
+	std::swap(vect[low], vect[right]);
+	return right;
+}
+
+int findOrderStatisticRecursive(std::vector<int>& vect, int k, int low, int high) {
+	if (high < low) {
+		return vect[low];
+	}
+
+	int pivot_pos = partition(vect, low, high);
+
+	if (pivot_pos < k - 1) {
+		findOrderStatisticRecursive(vect, k, pivot_pos + 1, high);
+	}
+	else {
+		findOrderStatisticRecursive(vect, k, low, pivot_pos - 1);
 	}
 }
 
-void mergeSortBottomUp(std::vector<struct HAMSTR> &array) {
-	int n = (int)array.size();
-	std::vector<struct HAMSTR> aux(n);
-
-	for (int stride = 1; stride < n; stride *= 2) {
-		for (int i = 0; i < n - stride; i += stride * 2) {
-			int left = i;
-			int middle = i + stride - 1;
-			int right = std::min(i + stride * 2 - 1, n - 1);
-			merge(array, aux, left, middle, right);
-		}
-	}
-}
-
-void insertionSort(std::vector<struct HAMSTR> &array) {
-	for (int i = 0; i < array.size(); i++) {
-		int j = i - 1;
-
-		while (j > 0 && array[j].foodTotal > array[i].foodTotal) {
-			std::swap(array[i], array[j]);
-		}
-	}
+void sortPartial(std::vector<int>& vect, int k) {
+	findOrderStatisticRecursive(vect, k, 0, vect.size() - 1);
 }
 
 struct OUTPUT_DATA solve(struct INPUT_DATA inputData) {
 
-	auto hamstrs = inputData.hamstrs;
+	std::vector<int> hamstrFoodTotal(inputData.hamstrCount);
 	int currentHamstrCount = 1;
 	int currentConsumedFood = 0;
 
 	while (true) {
 		for (int i = 0; i < inputData.hamstrCount; i++) {
-			hamstrs[i].foodTotal = hamstrs[i].foodRate +
-				(currentHamstrCount - 1) * hamstrs[i].foodGreed;
+			hamstrFoodTotal[i] = inputData.hamstrFoodRate[i] + 
+				(currentHamstrCount - 1) * inputData.hamstrFoodGreed[i];
 		}
 
-		if (currentHamstrCount < 2) {
-			mergeSortBottomUp(hamstrs);
-		}
-		else {
-			insertionSort(hamstrs);
-		}
+		sortPartial(hamstrFoodTotal, currentHamstrCount);
 
-		currentConsumedFood = getSum(hamstrs, currentHamstrCount);
-
+		currentConsumedFood = getSum(hamstrFoodTotal, currentHamstrCount);
 		if (currentConsumedFood > inputData.foodSupplay) {
 			currentHamstrCount--;
 			break;
@@ -140,7 +140,6 @@ void writeOutput(std::string fileName, struct OUTPUT_DATA outputData) {
 }
 
 int main(int argc, const char *argv[]) {
-	
 	auto inputFileName = argc == 1 ? "hamstr.in" : argv[1];
 	auto outputFileName = argc == 1 ? "hamstr.out" : argv[2];
 
